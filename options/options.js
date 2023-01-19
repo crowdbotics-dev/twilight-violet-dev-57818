395c96c1fe71e207383fe6e973a7d31214a84b12
Crowdbotics <engineering@crowdbotics.com>
2023-01-19 00:18:16 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "twilight-violet-dev-57818",
+  url: "https://twilight-violet-dev-57818.botics.co",
+  api: "https://twilight-violet-dev-57818.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
